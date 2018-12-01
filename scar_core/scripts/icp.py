4cf84e7d204450d923b31287d5bff4bed3127946
Jamie Cho <jchocholate@gmail.com>
2018-12-01 03:04:25 -0500

    icp characterization with subsampling

--- scar_core/scripts/icp.py
+++ scar_core/scripts/icp.py
@@ -1,17 +1,17 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-class icp():
-
+class ICP():
     def __init__(self):
-        x = 0
+        pass
 
-    def best_fit_transform(self, A, B):
+    @staticmethod
+    def best_fit_transform(A, B):
         '''
         Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
         Input:
-          A: Nxm numpy array of corresponding points
-          B: Nxm numpy array of corresponding points
+          A: Naxm numpy array of corresponding points
+          B: Nbxm numpy array of corresponding points
         Returns:
           T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
           R: mxm rotation matrix
@@ -49,22 +49,17 @@ class icp():
 
         return T, R, t
 
-
-    def nearest_neighbor(self, src, dst):
+    @staticmethod
+    def nearest_neighbor(src, dst):
         '''
         Find the nearest (Euclidean) neighbor in dst for each point in src
         Input:
-            src: Nxm array of points
-            dst: Nxm array of points
+            src: Naxm array of points
+            dst: Nbxm array of points
         Output:
             distances: Euclidean distances of the nearest neighbor
             indices: dst indices of the nearest neighbor
         '''
-
-        print '=========='
-        print src.shape
-        print dst.shape
-
         assert src.shape[1:] == dst.shape[1:]
 
         neigh = NearestNeighbors(n_neighbors=1)
@@ -72,18 +67,18 @@ class icp():
         distances, indices = neigh.kneighbors(src, return_distance=True)
         return distances.ravel(), indices.ravel()
 
-
-    def icp(self, A, B, init_pose=None, max_iterations=20, tolerance=0.001):
+    @staticmethod
+    def icp(A, B, init_pose=None, max_iterations=20, tolerance=0.001):
         '''
         The Iterative Closest Point method: finds best-fit transform that maps points A on to points B
         Input:
-            A: Nxm numpy array of source mD points
-            B: Nxm numpy array of destination mD point
+            A: Naxm numpy array of source mD points
+            B: Nbxm numpy array of destination mD point
             init_pose: (m+1)x(m+1) homogeneous transformation
             max_iterations: exit algorithm after max_iterations
             tolerance: convergence criteria
         Output:
-            T: final homogeneous transformation that maps A on to B
+            T: final homogeneous transformation that maps A on to B, T.A = B
             distances: Euclidean distances (errors) of the nearest neighbor
             i: number of iterations to converge
         '''
@@ -91,23 +86,17 @@ class icp():
         # assert A.shape == B.shape
 
         # get number of dimensions
-        ma = A.shape[1]
-        mb = B.shape[1]
 
+        ndim = A.shape[1]
+        nA, nB = A.shape[0], B.shape[0]
+        assert(A.shape[1:] == B.shape[1:])
 
         # make points homogeneous, copy them to maintain the originals
-        src = np.ones((ma+1,A.shape[0]))
-
-        print B.shape[0]
-        dst = np.ones((mb+1,B.shape[0]))
-
-        print src.shape
-        print dst.shape
-        print A.shape
-        print B.shape
+        src = np.ones((nA,ndim+1))
+        dst = np.ones((nB,ndim+1))
         
-        src[:ma,:] = A.T
-        dst[:mb,:] = B.T
+        src[:, :ndim] = A
+        dst[:, :ndim] = B
 
         # apply the initial pose estimation
         if init_pose is not None:
@@ -117,13 +106,13 @@ class icp():
 
         for i in range(max_iterations):
             # find the nearest neighbors between the current source and destination points
-            distances, indices = self.nearest_neighbor(src[:-1,:].T, dst[:-1,:].T)
+            distances, indices = ICP.nearest_neighbor(src[:,:-1], dst[:,:-1])
 
             # compute the transformation between the current source and nearest destination points
-            T,_,_ = self.best_fit_transform(src[:-1,:].T, dst[:-1,indices].T)
+            T,_,_ = ICP.best_fit_transform(src[:,:-1], dst[indices,:-1])
 
             # update the current source
-            src = np.dot(T, src)
+            src = np.dot(src,T.T) # right-multiply transform matrix
 
             # check error
             mean_error = np.mean(distances)
@@ -132,6 +121,91 @@ class icp():
             prev_error = mean_error
 
         # calculate final transformation
-        T,_,_ = self.best_fit_transform(A, src[:-1,:].T)
+        T,_,_ = ICP.best_fit_transform(A, src[:,:-1])
 
         return T, distances, i
+
+def Rmat(x):
+    c,s = np.cos(x), np.sin(x)
+    R = np.float32([c,-s,s,c]).reshape(2,2)
+    return R
+
+def get_line(n, s):
+    # return Nx2 line
+    t = np.linspace(0, s, n)
+    pt = np.stack([t, np.zeros_like(t)], axis=-1)
+    h = np.random.uniform(-np.pi, np.pi)
+    o = np.random.uniform(-1.0, 1.0)
+    return pt.dot(Rmat(h).T) + o
+
+def main():
+    from matplotlib import pyplot as plt
+    # parameters
+    n_size = 100
+    n_repeat = 10
+    s_noise = 0.1
+
+    pt_map = np.concatenate([get_line(n=33, s=1.0) for _ in range(3)], axis=0)
+
+    # get geometric transformation
+    # scan_{map} -> scan_{base_link}
+    h = np.random.uniform(-np.pi/4, np.pi/4)
+    R = Rmat(h)
+    t = np.random.uniform(-1.0, 1.0, size=(1,2))
+    T0 = np.eye(3)
+    T0[:2,:2] = R
+    T0[:2,2] = t
+
+    print ('T (x,y,h) : ({}, {}, {})'.format(t[0,0], t[0,1], h))
+
+    errs = []
+    #n_sub  = 20
+    n_subs = np.linspace(1, len(pt_map), num=10).astype(np.int32)
+    for n_sub in n_subs:
+        print('n_sub = {}'.format(n_sub))
+        err_i = []
+
+        for _ in range(n_repeat):
+            # subsample
+            pt_scan = np.copy(pt_map[np.random.choice(pt_map.shape[0], size=n_sub, replace=False)])
+
+            # apply transform
+            pt_scan = pt_scan.dot(R.T) + t
+
+            # apply noise
+            pt_scan = np.random.normal(loc=pt_scan, scale=s_noise)
+            T, distances, iterations = ICP.icp(pt_scan, pt_map,
+                    tolerance=0.000001)
+
+            pt_scan_r = pt_scan.dot(T[:2,:2].T) + T[:2,2].reshape(1,2)
+
+            # scan_{base_link} -> scan_{map}
+            h2 = np.arctan2(T[1,0], T[1,1])
+            t2 = T[:2,2]
+            #print ('T_inv (x,y,h) : ({}, {}, {})'.format(t2[0], t2[1], h2))
+
+            # compute error metric
+            #print T0.dot(T)
+            err_T = T0.dot(T)
+            dt = err_T[:2,2]
+            dh = np.arctan2(err_T[1,0], err_T[1,1])
+            err_i.append(np.abs([dt[0], dt[1], dh]))
+            #print('err (dx,dy,dh) : ({},{},{})'.format(dt[0],dt[1],dh))
+        err_i = np.mean(err_i, axis=0)
+        errs.append(err_i)
+
+        #plt.plot(pt_map[:,0], pt_map[:,1], '.', label='map')
+        #plt.plot(pt_scan[:,0], pt_scan[:,1], '.', label='scan')
+        #plt.plot(pt_scan_r[:,0], pt_scan_r[:,1], '.', label='scan-T')
+        #plt.legend()
+        #plt.show()
+
+    plt.plot(np.float32(n_subs) / len(pt_map), errs)
+    plt.legend(['dx','dy','dh'])
+    plt.xlabel('sample ratio')
+    plt.ylabel('error')
+    plt.title('icp characterization')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
